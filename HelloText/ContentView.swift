e5c7bb3566a8d1529dbe6a797b1644e58b74d9f2
Ayesha Ilyas <annie16950@gmail.com>
2022-02-19 13:50:17 -0500

    Initial Commit

---
+++ HelloText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloText
+//
+//  Created by Ayesha Ilyas on 2/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloText/HelloTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTextApp.swift
+//  HelloText
+//
+//  Created by Ayesha Ilyas on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
